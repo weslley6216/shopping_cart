ed02776c8741557f415b80528ef11680882e9f84
Weslley Campos <weslley.campos@rebase.com.br>
2025-05-13 13:20:15 -0300

    Add dependent destroy for cart model

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
   has_many :products, through: :cart_items
 
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
