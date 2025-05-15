f2bd9b1e1863fa6cb012db14345ef18da06456fa
Weslley Campos <weslley.campos@rebase.com.br>
2025-05-14 23:22:07 -0300

    Remove unnecessary parameter in remove item route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :products
   resource :cart, only: %i[create show] do
     post 'add_item', on: :collection
-    delete ':product_id', to: 'carts#remove_item', as: :remove_item
+    delete ':product_id', to: 'carts#remove_item'
   end
 
   get 'up' => 'rails/health#show', as: :rails_health_check
